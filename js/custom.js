577438b3466a140db85b281daa4a2847eca34fb9
wwwoojung <50447560+wwwoojung@users.noreply.github.com>
2023-12-27 18:13:55 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,23 @@
 const VISUAL_SLIDE = new Swiper('.visual_slide', {
     loop: true,
 });
+
+const MAIN_ESG_CON = document.querySelectorAll('.MainESG .ESG_con .con_box');
+// const MAIN_ESG_CON = document.querySelector('.MainESG .ESG_con .con_box');
+// MAIN_ESG_CON.addEventListener('mouseover', function () {
+//     MAIN_ESG_CON.classList.add('on');
+// })
+
+MAIN_ESG_CON.forEach((it) => {
+    it.addEventListener('mouseenter', () => {
+        for (var i = 0; i < MAIN_ESG_CON.length; i++) {
+            MAIN_ESG_CON[i].classList.remove('on');
+        }
+        it.classList.add('on');
+    })
+})
+
+const MBTN = document.querySelector('.mbtn')
+MBTN.addEventListener('click', () => {
+    document.querySelector('#Header .header_wrap .gnb').classList.toggle('on');
+})
