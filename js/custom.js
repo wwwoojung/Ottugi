2daf11076766fb5f7b31ecbf4139a03d1d396537
wwwoojung <50447560+wwwoojung@users.noreply.github.com>
2023-12-28 18:16:19 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,17 @@
+window.addEventListener('scroll', () => {
+    const SCT = window.scrollY;
+    if (SCT > 0) {
+        document.querySelector('#Header .header_wrap').classList.add('on')
+    } else {
+        document.querySelector('#Header .header_wrap').classList.remove('on')
+    }
+})
+
 const VISUAL_SLIDE = new Swiper('.visual_slide', {
     loop: true,
 });
 
 const MAIN_ESG_CON = document.querySelectorAll('.MainESG .ESG_con .con_box');
-// const MAIN_ESG_CON = document.querySelector('.MainESG .ESG_con .con_box');
-// MAIN_ESG_CON.addEventListener('mouseover', function () {
-//     MAIN_ESG_CON.classList.add('on');
-// })
-
 MAIN_ESG_CON.forEach((it) => {
     it.addEventListener('mouseenter', () => {
         for (var i = 0; i < MAIN_ESG_CON.length; i++) {
